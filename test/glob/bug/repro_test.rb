520e0ab2b2528515c3ed382be42fbdb4234a82c3
Tony Ta <tonyta.tt@gmail.com>
2018-12-19 23:04:45 -0800

    Install dependencies and make tests pass
    ```
    $ bundle install
    $ bundle exec rake
    Run options: --seed 1390
    
    # Running:
    
    .
    
    Finished in 0.000782s, 1278.7724 runs/s, 1278.7724 assertions/s.
    
    1 runs, 1 assertions, 0 failures, 0 errors, 0 skips
    ```

--- test/glob/bug/repro_test.rb
+++ test/glob/bug/repro_test.rb
@@ -4,8 +4,4 @@ class Glob::Bug::ReproTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Glob::Bug::Repro::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
