90671dd8bb997df6fc7c070e5da42ca45d195cb3
Tony Ta <tonyta.tt@gmail.com>
2018-12-19 23:03:38 -0800

    Initialize new gem with minitest via bundler
    ```
    $ bundle gem --test=minitest glob-bug-repro
    ```

---
+++ lib/glob/bug/repro.rb
@@ -0,0 +1,10 @@
+require "glob/bug/repro/version"
+
+module Glob
+  module Bug
+    module Repro
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/glob/bug/repro/version.rb
@@ -0,0 +1,7 @@
+module Glob
+  module Bug
+    module Repro
+      VERSION = "0.1.0"
+    end
+  end
+end
